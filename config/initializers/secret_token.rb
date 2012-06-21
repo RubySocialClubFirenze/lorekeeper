53f3eeb1456c563919c694c5de5c97b18e740020
Alessio Caiazza <nolith@abisso.org>
2012-06-21 17:17:59 +0200

    rails new lorekeeper -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lorekeeper::Application.config.secret_token = '5b7d3b14b3597bbb0229ed25f561e6d057c831b289d7c3c8fd760340177b278e3a2497be9ccc21fbfdb6a411011a47c6b67974f7c5798395543bde21b9632be1'
