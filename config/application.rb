52790e3b10c55ea4ce7e1267531f46c6eb6dbcce
Alessio Caiazza <nolith@abisso.org>
2012-06-21 17:33:21 +0200

    activeadmin setup

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Lorekeeper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #only for heroku
+    #config.assets.initialize_on_precompile = false
   end
 end
